0a0e08b2bd963d82aaca14e04613ede69055283a
Carlos Ucañan Barrera <cucaan@miatech.net>
2020-01-22 12:20:14 -0500

    preproduccion a heroku 2

--- index.js
+++ index.js
@@ -1,15 +1,22 @@
-var bodyParser = require('body-parser'),
+require('./server/config/config');
+const express = require('express');
+const app = express();
+const cors = require('cors');
+routing    = require('./server/requestRouting')
+/*var bodyParser = require('body-parser'),
     http       = require('http'),
     express    = require('express')
     routing    = require('./server/requestRouting')
 
-var port       = port = process.env.PORT || 8080,
+var port       = port = process.env.PORT || 3000,
     app        = express(),
     Server     = http.createServer(app)
-var path = require ('path')
+var path = require ('path')*/
+
+const bodyParser = require('body-parser');
 
 app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({extended: true}))
+app.use(bodyParser.urlencoded({extended: false}))
 app.use(express.static('./public'))
 app.get('/',function(req,res){
   res.sendFile(path.join(__dirname+'./public'))
@@ -17,6 +24,11 @@ app.get('/',function(req,res){
 })
 
 app.use('/bdjson', routing)
-Server.listen(port, () => {
+/*Server.listen(port, () => {
   console.log("Servidor corriendo en el puerto: " + port)
-})
+})*/
+
+app.listen(process.env.PORT, async () => {
+  console.log('Escuchando puerto: ', process.env.PORT);
+  
+});
